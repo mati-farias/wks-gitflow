6f163bcecafba8086bc8b272b7a3479bc57cba31
mati-farias <mati.farias91@hotmail.com>
2022-07-13 16:11:45 -0300

    People table finished

--- arturito/src/utils/fetcher.ts
+++ arturito/src/utils/fetcher.ts
@@ -4,5 +4,9 @@ import axios from 'axios';
 // const baseURL = 'https://www.swapi.it/api';
 const baseURL = 'https://swapi.dev/api';
 
+
 export const swGet = (url: string) =>
   axios.get(url, { baseURL }).then((res) => res.data);
+
+export const pplGet = (url: string) =>
+  axios.get(url, { baseURL }).then((res) => res.data)
